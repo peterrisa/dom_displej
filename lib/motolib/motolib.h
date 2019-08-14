973066bb4f1da651f9a8068fe18ab737aee3168b
Pavol Risa <risapav@gmail.com>
2019-08-14 16:14:28 +0200

    added motolib

---
+++ lib/motolib/motolib.h
@@ -0,0 +1,186 @@
+#ifndef __MOTOLIB_HANDLER_H__
+#define __MOTOLIB_HANDLER_H__
+
+#include <Arduino.h>
+#include <assert.h>
+
+//Arduino H mostík pro krokový motor L298N Dual H Most DC
+//Here’s a table that summarizes the pins and corresponding motor direction. These assumes you are following the same Fritzing diagram above.
+/*
+motor A motor B
+------- -------
+IN1	IN2	IN3	IN4	Direction
+0	0	0	0	Stop
+1	0	1	0	Forward
+0	1	0	1	Reverse
+1	0	0	1	Left
+0	1	1	0	Right
+*/
+
+/*
+HW connection of motors
+DC motor 1 "+" or stepper motor A+
+DC motor 1 "-" or stepper motor A-
+12V jumper - remove this if using a supply voltage greater than 12V DC. This enables power to the onboard 5V regulator
+Connect your motor supply voltage here, maximum of 35V DC. Remove 12V jumper if >12V DC
+GND
+5V output if 12V jumper in place, ideal for powering your Arduino (etc)
+DC motor 1 enable jumper. Leave this in place when using a stepper motor. Connect to PWM output for DC motor speed control.
+IN1
+IN2
+IN3
+IN4
+DC motor 2 enable jumper. Leave this in place when using a stepper motor. Connect to PWM output for DC motor speed control.
+DC motor 2 "+" or stepper motor B+
+DC motor 2 "-" or stepper motor B-
+*/
+
+class LM298N_bridge {
+	public:
+		// constructor
+		LM298N_bridge(){
+			//define arduino pins
+			// motor one is "A"
+			int _enA = 0;
+			int _in1 = 0;
+			int _in2 = 0;
+			// motor two is "B"
+			int _enB = 0;
+			int _in3 = 0;
+			int _in4 = 0;
+			//preset internal statuses
+			_stopA = true; // true> stopped, false> run
+			_stopB = true; // true> stopped, false> run
+			_directionA = true;// true> cw, false> ccw
+			_directionB = true;// true> cw, false> ccw
+			_speedA = 0; // PWM preset from 0 to 255
+			_speedB = 0; // PWM preset from 0 to 255
+		};
+		//setup arduino, initialize driver
+		setup(enA, in1, in2, enB, in3, in4){
+			assert(enA == 0 || in1 == 0 || in2 == 0);
+			assert(enB == 0 || in3 == 0 || in4 == 0);
+			// set all the motor control pins to outputs
+			pinMode(_enA, OUTPUT);
+			pinMode(_enB, OUTPUT);
+			pinMode(_in1, OUTPUT);
+			pinMode(_in2, OUTPUT);
+			pinMode(_in3, OUTPUT);
+			pinMode(_in4, OUTPUT);
+		};
+		// change direction true = cw, false = ccw
+		void setDirection(bool dir){
+			setDirectionA(dir);
+			setDirectionB(dir);
+		};
+		void setDirectionA(bool dir){
+			if(_directionA != dir) {
+				stopA();
+				_directionA = dir;
+				if(_direction) {
+					digitalWrite(_in1, HIGH);
+					digitalWrite(_in2, LOW);
+				} else{
+					digitalWrite(_in1, LOW);
+					digitalWrite(_in2, HIGH);					
+				}
+			}
+		};
+		void setDirectionB(bool dir){
+			if(_directionB != dir) {
+				stopB();
+				_directionB = dir;
+				if(_direction) {
+					digitalWrite(_in3, HIGH);
+					digitalWrite(_in4, LOW);
+				} else{
+					digitalWrite(_in3, LOW);
+					digitalWrite(_in4, HIGH);					
+				}
+			}
+		};
+		// preset speed
+		void setSpeed(unsigned char speed){
+			setSpeedA(speed);
+			setSpeedB(speed);
+		};
+		void setSpeedA(unsigned char speed){
+			assert(_enA == 0);
+			_speedA = speed;
+			analogWrite(_enA, _speedA);
+		};
+		void setSpeedB(unsigned char speed){
+			assert(_enB == 0);
+			_speedA = speed;
+			analogWrite(_enB, _speedA);
+		};
+		// start rotating
+		void start(){
+			startA();
+			startB{};
+		};
+		void startA(){
+			assert(_in1 == 0 || _in2 == 0);
+			setSpeedA(_speedA);
+			if(_directionA) {
+				digitalWrite(_in1, HIGH);
+				digitalWrite(_in2, LOW);
+			} else{
+				digitalWrite(_in1, LOW);
+				digitalWrite(_in2, HIGH);					
+			}
+			_stopA = false;
+		};
+		void startB(){
+			assert(_in3 == 0 || _in4 == 0);
+			
+			setSpeedB(_speedB);
+			if(_directionA) {
+				digitalWrite(_in3, HIGH);
+				digitalWrite(_in4, LOW);
+			} else{
+				digitalWrite(_in3, LOW);
+				digitalWrite(_in4, HIGH);					
+			}
+			_stopB = false;
+		};
+		// stop rotating both motors
+		void stop(){
+			stopA();
+			stopB();
+		};
+		void stopA(){
+			assert(_enA || _in1 == 0 || _in2 == 0);
+			// now turn off motor A
+			analogWrite(_enA, 0);
+			digitalWrite(_in1, LOW);
+			digitalWrite(_in2, LOW); 
+			_stopA = true;
+		};
+		void stopB(){
+			assert(_enB || _in3 == 0 || _in4 == 0);
+			// now turn off motor B
+			analogWrite(_enB, 0);
+			digitalWrite(_in3, LOW);
+			digitalWrite(_in4, LOW);
+			_stopB = true;
+		};
+	private:
+		// connect motor controller pins to Arduino digital pins
+		// motor one is "A"
+		int _enA, _in1, _in2;
+		// motor two is "B"
+		int _enB;
+		int _in3;
+		int _in4;
+		
+		//motor driver internal statuses
+		bool _stopA; // true> stopped, false> run
+		bool _directionA;// true> cw, false> ccw
+		unsigned char _speedA; // PWM preset from 0 to 255
+		bool _stopB; // true> stopped, false> run
+		bool _directionB;// true> cw, false> ccw
+		unsigned char _speedB; // PWM preset from 0 to 255		
+}
+
+#endif
---
+++ lib/motolib/motolib.ino
@@ -0,0 +1,114 @@
+#include <Arduino.h>
+#include "motolib.h"
+//-----------------------------------------------------------
+// prekontroluje ci je telegram validny
+// vrati true ak je validny, false ak nevalidny
+bool isValidTelegram(const TELEGRAM *msg) {
+  if (msg->stx != STX)
+    return false;
+  if (msg->etx != ETX)
+    return false;
+  return true;
+}
+
+// prekonvertuje telegram do char retazca ukonceneho '\x00'
+// buffer musi mat velkost 2*sizeof(TELEGRAM) + 1 bajtov
+// buffer - pole znakov kam sa bude konvertovat
+// msg - struktura telegramu
+// len - velkost struktury telegramu v bajtoch; sizeof(TELEGRAM)
+void encodeTelegram(byte *buffer, const TELEGRAM *msg, const int len) {
+  byte *b_msg = (byte *)msg;
+  byte *buf = buffer;
+  byte a, b;
+
+  for (int i = 0; i < len; i++, b_msg++) {
+    a = *b_msg & 0x0F;
+    b = (*b_msg & 0xF0) >> 4;
+    *buf++ = a + ADD;
+    *buf++ = b + ADD;
+  }
+  *buf = '\x00';
+}
+
+// prekonvertuje char retazec ukonceny '\x00' na telegram
+// buffer musi mat velkost 2*sizeof(TELEGRAM) + 1 bajtov
+// buffer - pole znakov odkial sa bude konvertovat
+// msg - struktura telegramu
+// len - velkost struktury telegramu v bajtoch; int len = sizeof(TELEGRAM);
+void decodeTelegram(TELEGRAM *msg, const int len, const byte *buffer) {
+  byte *b_msg = (byte *)msg;
+  byte *buf = buffer;
+  byte a, b;
+
+  for (int i = 0; i < len; i++, b_msg++) {
+    a = *buf++ - ADD;
+    b = *buf++ - ADD;
+    *b_msg = (a & 0x0F) | ((b & 0x0F) << 4);
+  }
+}
+
+void test() {
+  Serial.print("velkost F_VALUE ma byt 4: ");
+  Serial.println(sizeof(F_VALUE));
+  Serial.print("velkost B_VALUE ma byt 2: ");
+  Serial.println(sizeof(B_VALUE));
+  Serial.print("velkost TELEGRAM ma byt 26: ");
+  Serial.println(sizeof(TELEGRAM));
+
+  TELEGRAM msg_orig;
+  msg_orig.stx = STX;
+  msg_orig.etx = ETX;
+  msg_orig.v_1.v = -99.99;
+  msg_orig.v_2.v = 99.99;
+  msg_orig.v_3.v = -25.25;
+  msg_orig.v_4.v = 25.25;
+  msg_orig.b_1.b[0] = 0xAA;
+  msg_orig.b_1.b[1] = 0x55;
+  msg_orig.b_2.b[0] = 0xAA;
+  msg_orig.b_2.b[1] = 0x55;
+  msg_orig.b_3.b[0] = 0xAA;
+  msg_orig.b_3.b[1] = 0x55;
+  msg_orig.b_4.b[0] = 0xAA;
+  msg_orig.b_4.b[1] = 0x55;
+
+  char buffer[2 * sizeof(TELEGRAM) + 1];
+  encodeTelegram(buffer, &msg_orig, sizeof(msg_orig));
+
+  Serial.print("zakodovany telegram: ");
+  Serial.println(buffer);
+  Serial.print("odkodovany telegram: ");
+  Serial.println((char *)&msg_orig);
+
+  TELEGRAM msg_copy;
+  decodeTelegram(&msg_copy, sizeof(msg_copy), buffer);
+  Serial.print("odkodovany telegram: ");
+  Serial.println((char *)&msg_copy);
+
+  byte *orig = (byte *)&msg_orig;
+  byte *copy = (byte *)&msg_copy;
+
+  for (int i = 0; i < sizeof(TELEGRAM); i++) {
+    if (orig[i] == copy[i]) {
+      Serial.print("   zhoda  : ");
+      Serial.println(i);
+    } else {
+      Serial.print("!! nezhoda: ");
+      Serial.print(orig[i]);
+      Serial.print(" : ");
+      Serial.print(copy[i]);
+      Serial.print(" : ");
+      Serial.println(i);
+    }
+  }
+  Serial.print("   msg_orig je validny  : ");
+  Serial.println(isValidTelegram(&msg_orig));
+  Serial.print("   msg_copy je validny  : ");
+  Serial.println(isValidTelegram(&msg_copy));
+}
+
+void setup() {
+  Serial.begin(115200); // opens serial port
+  test();
+}
+
+void loop() {}
